1b2bf796e48fbe089813be754630262ea6c86ecb
Ricardo <rjmchiste@gmail.com>
2025-07-22 11:59:17 -0300

    Finalização do projeto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tls_client
 from lxml import html
 from bs4 import BeautifulSoup
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
 
 team_adress_A = {
     'red bull bragantino': '1999',
@@ -8,7 +11,7 @@ team_adress_A = {
     'palmeiras': '1963',
     'botafogo': '1958',
     'fluminense': '1961',
-    'atlético mineiro': '1977',
+    'atletico mineiro': '1977',
     'corinthians': '1957',
     'ceará': '2001',
     'mirassol': '21982',
@@ -50,7 +53,7 @@ team_adress_B={
 
 base_api = 'https://www.sofascore.com/api/v1/team/'
 end_api = '/statistics/overall'
-#1954/unique-tournament/325/season/58766/
+
 
 def choose_team(time: str):
     data_list = []
@@ -82,7 +85,7 @@ def choose_team(time: str):
 
     for url in urls_list:
         session = tls_client.Session(client_identifier="chrome_115")
-        api_link = session.get(url_25, allow_redirects=True).json()
+        api_link = session.get(url, allow_redirects=True).json()
         if not 'error' in api_link:
             data_list.append(api_link['statistics'])
             if urls_list.index(urls_list[cont_url_list]) == 0:
@@ -94,6 +97,58 @@ def choose_team(time: str):
             cont_data_list += 1
         cont_url_list+=1
     return data_list
+
+def build_dataframe(time:str):
+    team = choose_team(time.lower())
+    team_dataframe = pd.DataFrame(index=team[0].keys())
+
+    for i in range(len(team)):
+        team_dataframe[str(team[i]['ano'])] = team[i].values()
+        team_dataframe[str(team[i]['ano'])] = team_dataframe[str(team[i]['ano'])].apply(lambda x: float('{:.1f}'.format(x)))
+    
+    team_dataframe['Média'] = team_dataframe.mean(axis = 1).apply(lambda x: float('{:.1f}'.format(x)))
+
+    return team_dataframe
+
+def build_chart(team1: str, team2: str, metric:str):
+    df_team1 = build_dataframe(team1.lower())
+    df_team2 = build_dataframe(team2.lower())
+
+    anos = ['2023', '2024', '2025']
+    
+    fig = px.Figure(
+        data = [
+            px.Bar(name=team1, x=anos, y=[df_team1['2023'][metric], df_team1['2024'][metric], df_team1['2025'][metric]]),
+            px.Bar(name=team2, x=anos, y=[df_team2['2023'][metric], df_team2['2024'][metric], df_team2['2025'][metric]])
+
+        ],
+        layout_title_text = metric
+    )
+
+    return fig.show()
+
+def build_chart2(team1: str, metric: str):
+    df_team1 = build_dataframe(team1.lower())  # Supondo que retorna um dicionário
+
+    # Organizando os dados em um DataFrame para o plotly
+    anos = ['2023', '2024', '2025']
+    valores = [df_team1['2023'][metric], df_team1['2024'][metric], df_team1['2025'][metric]]
+    
+    df_plot = pd.DataFrame({
+        'Ano': anos,
+        metric: valores
+    })
+
+    fig = px.scatter(df_plot, x='Ano', y=metric, title=f"{metric} - {team1.title()}", trendline='ols')  # 'ols' = regressão linear
+
+    fig.show()
+
+
+
+
+
+
+
 
 '''#Serve para pegar os nomes e os códigos dos time
 session = tls_client.Session(client_identifier="chrome_115")
